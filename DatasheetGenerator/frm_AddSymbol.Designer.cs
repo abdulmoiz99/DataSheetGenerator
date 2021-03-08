c52f2fe115621bb60003d1ff86257a83d91c945b
Abdul Moiz <moiza0799@gmail.com>
2021-03-08 18:20:33 +0500

    Minor Fixes

---
+++ DatasheetGenerator/frm_AddSymbol.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace DatasheetGenerator
+{
+    partial class frm_AddSymbol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_AddSymbol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 518);
+            this.Name = "frm_AddSymbol";
+            this.Text = "frm_AddSymbol";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
